d8a83a2c10d3edfda9637f5c174482d38781b1e6
PPT-GitHub1 <git1@atter.jp>
2018-09-21 18:07:53 +0900

    master

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Class1
+    {
+        public string GetDate()
+        {
+            DateTime date = DateTime.Now;
+            string ret = date.ToShortDateString();
+            return (ret);
+        }
+
+        public string GetTime()
+        {
+            DateTime time = DateTime.Now;
+            string ret = time.ToShortTimeString();
+            return (ret);
+        }
+
+    }
+}
